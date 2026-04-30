001c09683bd562be40bf7d8b572377706178918b
F000NK <f000nk@voluntas-progressus.tech>
2026-04-30 23:04:57 +0600

    Release Mine v.1.5.0

---
+++ apps/packages/mine/1.5.0/manifest.lua
@@ -0,0 +1,13 @@
+return {
+    name = "mine",
+    version = "1.4.0",
+    description = "Advanced turtle miner: shaft/sectors/ore mode, RPC ACL-aware, plus cancellable jobs.",
+    author = "F000NKKK",
+    roles = { "turtle" },
+    deps = {},
+    requires = {},
+    permissions = { "rpc", "turtle", "fuel", "inventory", "gps" },
+    min_platform = "0.9.4",
+    entry = "main.lua",
+    files = { "main.lua" },
+}
